e7cb88f751bc8a187c21ec3af5c573ac38ca7e98
ArthurMargarian <Artur.Margaryan@volo.global>
2020-05-12 18:01:52 +0400

    login page, loader comp, placeholder directive

--- src/app/guest/components/login/login.component.ts
+++ src/app/guest/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  private loginForm: FormGroup;
+  private errorMessage: string;
+  private showLoader: boolean;
+  private invalidForm: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private titleService: Title,
+    private router: Router
+  ) {
+    this.titleService.setTitle('Login • Events');
   }
 
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email]
+      ),
+      password: new FormControl(
+        '',
+        [Validators.required, Validators.minLength(6)]
+      )
+    });
+  }
+
+  loginSubmit() {
+    this.showLoader = true;
+    const formData = {...this.loginForm.value};
+    if (this.loginForm.valid) {
+      console.log('auth logic');
+    } else {
+      this.invalidForm = true;
+      this.loginForm.controls.password.reset();
+      this.showLoader = false;
+      this.errorMessage = this.loginForm.controls.email.invalid ? 'Your email is invalid' : 'Your password is invalid';
+    }
+  }
+
+  onFormChanges() {
+    this.invalidForm = Boolean(this.loginForm.touched && this.loginForm.invalid);
+    this.errorMessage = '';
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,25 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import { SingleEventComponent } from './components/single-event/single-event.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PlaceholderTranslateDirective } from './directives/placeholder-translate.directive';
 
 @NgModule({
-  declarations: [SingleEventComponent],
+  declarations: [SingleEventComponent, LoaderComponent, PlaceholderTranslateDirective],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+  ],
+  exports: [
+    SingleEventComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LoaderComponent,
+    PlaceholderTranslateDirective,
   ]
 })
 export class SharedModule { }
