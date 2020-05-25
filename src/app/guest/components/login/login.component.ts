1b8b9b7ec5da8073427bad4840756c7b474bad3a
ArthurMargarian <Artur.Margaryan@volo.global>
2020-05-26 02:08:07 +0400

    login component -> structural clean up

--- src/app/guest/components/login/login.component.ts
+++ src/app/guest/components/login/login.component.ts
@@ -5,69 +5,74 @@ import {Router} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
 import {AuthService} from '../../../services/auth.service';
 import {ILoginRes} from '../../../shared/interfaces';
-import { ToastrService} from 'ngx-toastr';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  private loginForm: FormGroup;
-  private showLoader: boolean;
-  private validCredentials = true;
-  private submitted: boolean;
+    private loginForm: FormGroup;
+    private validCredentials = true;
+    private submitted: boolean;
 
-  constructor(
-    private titleService: Title,
-    private authService: AuthService,
-    private router: Router,
-    private toastrService: ToastrService
-  ) {
-    this.titleService.setTitle('Login • Events');
-  }
-
-  ngOnInit() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.required, Validators.email]
-      ),
-      password: new FormControl(
-        '',
-        [Validators.required]
-      )
-    });
-  }
-
-  loginSubmit() {
-    this.submitted = true;
-    const formData = {...this.loginForm.value};
-    if (this.loginForm.valid) {
-      this.validCredentials = true;
-      this.authService.login(formData)
-        .subscribe(
-          (res: ILoginRes) => {
-            localStorage.setItem('token', res.access_token);
-            localStorage.setItem('fullName', `${res.user.srName} ${res.user.name}`);
-            this.showLoader = false;
-            if (res.user.isAdmin) {
-              localStorage.setItem('isAdmin', 'admin');
-              this.router.navigate(['events-table']);
-            } else {
-              this.router.navigate(['events-grid']);
-            }
-          },
-          (error: HttpErrorResponse) => {
-            this.validCredentials = false;
-            this.toastrService.error(null, error.error.msg);
-          });
-    } else {
-      this.showLoader = false;
+    constructor(
+        private titleService: Title,
+        private authService: AuthService,
+        private router: Router,
+        private toastrService: ToastrService
+    ) {
     }
-  }
 
-   public onFormChanges():void {
-     this.validCredentials = true;
-  }
+    ngOnInit() {
+        this.titleService.setTitle('Login • Events');
+        this.initForm();
+    }
+
+    public onSubmit(): void {
+        this.submitted = true;
+        if (this.loginForm.valid) {
+            this.submitted = true;
+            this.validCredentials = true;
+            const formData = {...this.loginForm.value};
+            this.userLogin(formData);
+        }
+    }
+
+    public onFormChanges(): void {
+        this.validCredentials = true;
+    }
+
+    private initForm() {
+        this.loginForm = new FormGroup({
+            email: new FormControl(
+                '',
+                [Validators.required, Validators.email]
+            ),
+            password: new FormControl(
+                '',
+                [Validators.required]
+            )
+        });
+    }
+
+    private userLogin(formData): void {
+        this.authService.login(formData)
+            .subscribe(
+                (res: ILoginRes) => {
+                    localStorage.setItem('token', res.access_token);
+                    localStorage.setItem('fullName', `${res.user.srName} ${res.user.name}`);
+                    if (res.user.isAdmin) {
+                        localStorage.setItem('isAdmin', 'admin');
+                        this.router.navigate(['events-table']);
+                    } else {
+                        this.router.navigate(['events-grid']);
+                    }
+                },
+                (error: HttpErrorResponse) => {
+                    this.validCredentials = false;
+                    this.toastrService.error(error.error.msg, 'Log In');
+                });
+    }
 }
